c8a7ade4d11c96c95e0bb4476d5fc80c5c46099b
Stellawww <yuqiw62@g.ucla.edu>
2021-10-24 20:39:46 -0700

    modified variable names

--- IMDB_scraper/IMDB_scraper/settings.py
+++ IMDB_scraper/IMDB_scraper/settings.py
@@ -16,7 +16,7 @@ NEWSPIDER_MODULE = 'IMDB_scraper.spiders'
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'IMDB_scraper (+http://www.yourdomain.com)'
 
-CLOSESPIDER_PAGECOUNT = 20
+#CLOSESPIDER_PAGECOUNT = 20
 
 
 # Obey robots.txt rules
